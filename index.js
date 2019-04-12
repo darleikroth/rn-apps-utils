f68436f4e3278bab164ecd98950e985c197e769f
Darlei Kroth <maildarlei@icloud.com>
2019-04-12 10:29:00 -0300

    feat: add decimal formatter, pushID generator, encryptSHA

--- index.js
+++ index.js
@@ -1,4 +1,33 @@
+import decimal from './lib/decimal'
+import encrypt from './lib/encryptSHA'
+import generatePushID from './lib/generatePushID'
+
 export default {
+  decimal(num: number, decimals: number) {
+    return decimal(num, decimals)
+  },
+
+  decimalBRL(num: number) {
+    return `R$${decimal(num, 2)}`
+  },
+
+  decimalKG(num: number, decimals: number) {
+    return `${decimal(num, decimals)} KG`
+  },
+
+  decimalBank(num: number) {
+    num = num / 100
+    return decimal(num, 2)
+  },
+
+  encryptSHA(text: string) {
+    return encrypt.hash(text, { outFormat: 'hex' })
+  },
+
+  generatePushID() {
+    return generatePushID()
+  },
+
   isIphoneX({ height, width }, ios = true) {
     return ios && ((height === 812 || width === 812) || (height === 896 || width === 896))
   },

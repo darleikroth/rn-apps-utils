791a16a3b22166167e36943d8f629e332f7dc239
Darlei Kroth <maildarlei@icloud.com>
2019-08-09 14:22:38 -0300

    feat: add email validator

--- index.js
+++ index.js
@@ -1,38 +1,43 @@
-import decimal from './lib/decimal'
-import encrypt from './lib/encryptSHA'
-import generatePushID from './lib/generatePushID'
+import decimal from './lib/decimal';
+import encrypt from './lib/encryptSHA';
+import generatePushID from './lib/generatePushID';
+import validateEmail from './lib/validateEmail';
 
 export default {
   decimal(num: number, decimals: number) {
-    return decimal(num, decimals)
+    return decimal(num, decimals);
   },
 
   decimalBRL(num: number) {
-    return `R$${decimal(num, 2)}`
+    return `R$${decimal(num, 2)}`;
   },
 
   decimalKG(num: number, decimals: number) {
-    return `${decimal(num, decimals)} KG`
+    return `${decimal(num, decimals)} KG`;
   },
 
   decimalBank(num: number) {
-    num = num / 100
-    return decimal(num, 2)
+    num = num / 100;
+    return decimal(num, 2);
   },
 
   encryptSHA(text: string) {
-    return encrypt.hash(text, { outFormat: 'hex' })
+    return encrypt.hash(text, { outFormat: 'hex' });
   },
 
   generatePushID() {
-    return generatePushID()
+    return generatePushID();
   },
 
   isIphoneX({ height, width }, ios = true) {
-    return ios && ((height === 812 || width === 812) || (height === 896 || width === 896))
+    return ios && ((height === 812 || width === 812) || (height === 896 || width === 896));
   },
 
   isOrientationLandscape({ width, height }) {
-    return width > height
-  }
+    return width > height;
+  },
+
+  validateEmail(email = '') {
+    return validateEmail(email);
+  },
 }
